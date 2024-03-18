55a0b017c98e429419e8ec9dce85927baabd8171
kushan sewmina <115915441+kushansewmina1234@users.noreply.github.com>
2024-03-18 10:54:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "",
+    SESSION_ID: process.env.SESSION_ID || "DARKSHAN=EnclosedLived",
     DATABASE_URL: "postgresql://aswin:kUznbwKXY4YlS9jnTUpS6Q@lava-primate-8185.8nk.gcp-asia-southeast1.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full",
     PREFIX: "^",
     MODE: "public",
